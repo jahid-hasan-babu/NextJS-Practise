134b6ff58b23efc538c8f0f306ba2a3441ba713d
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-01-11 00:40:02 +0600

    intro

---
+++ Intro/Intro.js
@@ -0,0 +1,22 @@
+/*
+NextJS is a react framework 
+it uses React for building user interface
+Provide additional features that enable you to build production ready applications
+We don`t need to install additional packages as NextJs Provide everything we need
+
+
+Why we learn NextJS
+
+1.Routing
+2.API routes
+3.Rendering
+4.Data fetching
+5.Styling
+6.Optimization
+7.Dev and prod build system
+
+Requirements
+Html, CSS, JS, ReactJS
+
+
+*/
