63ac58d14f03175b4a98b6e5d07d3b7c9169bd2f
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-09-08 11:43:20 +0600

    array-type-done

---
+++ typeScript/user-define-type/array-type.js
@@ -0,0 +1,9 @@
+var userPhone;
+// let userName: string[];
+var userName;
+userName = ["Jahid", "Hasan", "Babu"];
+console.log(userName[1]);
+var multipleType; // multiple type array
+multipleType = [1, "Jahid"];
+userName.sort();
+console.log(userName);
---
+++ typeScript/user-define-type/array-type.ts
@@ -0,0 +1,13 @@
+let userPhone: number[];
+// let userName: string[];
+let userName: Array<string>;
+
+userName = ["Jahid", "Hasan", "Babu"];
+console.log(userName[1]);
+
+let multipleType: (string | number)[]; // multiple type array
+
+multipleType = [1, "Jahid"];
+
+userName.sort();
+console.log(userName);
