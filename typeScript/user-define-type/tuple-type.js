2df7db7a8a008e6d3f23da4136aaaa60cbeb53cc
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-09-08 11:49:45 +0600

    tuple-type

---
+++ typeScript/user-define-type/tuple-type.js
@@ -0,0 +1,6 @@
+//mixed data -- key, value
+var user;
+user = [101, "anis"];
+user.push(102, "babu");
+console.log(user);
+console.log(user[1]);
---
+++ typeScript/user-define-type/tuple-type.ts
@@ -0,0 +1,8 @@
+//mixed data -- key, value
+let user: [number, string];
+
+user = [101, "anis"];
+user.push(102, "babu");
+
+console.log(user);
+console.log(user[1]);
