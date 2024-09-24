eb67b5de53556e1eab8819558461724b59097f86
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-09-24 14:22:56 +0600

    thapa-ts

---
+++ Thapa/index.js
@@ -0,0 +1,11 @@
+// console.log("My name is jahid");
+// let num:number = 5;
+//
+//
+// console.log(num)
+//function define
+//@ts-ignore
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 12));
---
+++ Thapa/index.ts
@@ -0,0 +1,27 @@
+// console.log("My name is jahid");
+// let num:number = 5;
+//
+//
+// console.log(num)
+
+
+//function define
+//@ts-ignore
+function sum(a:number, b:number):number{
+    return a + b;
+}
+
+console.log(sum(5,12))
+
+
+
+
+
+
+
+
+
+
+
+
+
